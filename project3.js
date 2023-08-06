6f07b1147ce14d1c7edba83da762947df360a292
Ajeet Kumar <70510740+ajeetbkj@users.noreply.github.com>
2023-08-06 20:53:17 +0530

    project3.js

---
+++ project3.js
@@ -0,0 +1,408 @@
+const foodData = [{
+  
+  "foodname": "Apple",
+  "calorie": 95,
+  "category": "Fruit",
+  "protiens": 0.3,
+  "cab": 25
+},
+{
+ 
+  "foodname": "Banana",
+  "calorie": 105,
+  "category": "Fruit",
+  "protiens": 1.3,
+  "cab": 27
+},
+{
+  
+  "foodname": "Orange",
+  "calorie": 62,
+  "category": "Fruit",
+  "protiens": 1.2,
+  "cab": 15
+},
+{
+  
+  "foodname": "Pear",
+  "calorie": 95,
+  "category": "Fruit",
+  "protiens": 0.5,
+  "cab": 25
+},
+{
+  
+  "foodname": "Grapefruit",
+  "calorie": 42,
+  "category": "Fruit",
+  "protiens": 1.1,
+  "cab": 11
+},
+{
+  
+  "foodname": "Strawberry",
+  "calorie": 46,
+  "category": "Fruit",
+  "protiens": 1.5,
+  "cab": 10
+},
+{
+  
+  "foodname": "Blueberry",
+  "calorie": 52,
+  "category": "Fruit",
+  "protiens": 1.1,
+  "cab": 14
+},
+{
+  
+  "foodname": "Raspberry",
+  "calorie": 52,
+  "category": "Fruit",
+  "protiens": 1.4,
+  "cab": 15
+},
+{
+  
+  "foodname": "Broccoli",
+  "calorie": 34,
+  "category": "Vegetable",
+  "protiens": 3.3,
+  "cab": 5
+},
+{
+  
+  "foodname": "Cauliflower",
+  "calorie": 25,
+  "category": "Vegetable",
+  "protiens": 2.6,
+  "cab": 5
+},
+{
+  
+  "foodname": "Green Beans",
+  "calorie": 31,
+  "category": "Vegetable",
+  "protiens": 2.4,
+  "cab": 4
+},
+{
+  
+  "foodname": "Asparagus",
+  "calorie": 20,
+  "category": "Vegetable",
+  "protiens": 2.2,
+  "cab": 2
+},
+{
+  
+  "foodname": "Spinach",
+  "calorie": 23,
+  "category": "Vegetable",
+  "protiens": 3.5,
+  "cab": 4
+},
+{
+
+  "foodname": "Kale",
+  "calorie": 33,
+  "category": "Vegetable",
+  "protiens": 4.3,
+  "cab": 7
+},
+{
+  
+  "foodname": "Sweet Potato",
+  "calorie": 103,
+  "category": "Vegetable",
+  "protiens": 2,
+  "cab": 27
+},
+{
+  
+  "foodname": "Potato",
+  "calorie": 161,
+  "category": "Vegetable",
+  "protiens": 4.3,
+  "cab": 37
+},
+{
+  "foodname": "Carrot",
+  "calorie": 41,
+  "category": "Vegetable",
+  "protiens": 0.9,
+  "cab": 9
+},
+{
+  
+  "foodname": "Onion",
+  "calorie": 40,
+  "category": "Vegetable",
+  "protiens": 1.4,
+  "cab": 9
+},
+{
+  
+  "foodname": "Egg",
+  "calorie": 77,
+  "category": "Protein",
+  "protiens": 6.3,
+  "cab": 0.5
+},
+{
+ 
+  "foodname": "Chicken Breast",
+  "calorie": 165,
+  "category": "Protein",
+  "protiens": 31,
+  "cab": 0
+},
+{
+ 
+  "foodname": "Salmon",
+  "calorie": 206,
+  "category": "Protein",
+  "protiens": 22,
+  "cab": 0
+},
+{
+  
+  "foodname": "Tuna",
+  "calorie": 179,
+  "category": "Protein",
+  "protiens": 39,
+  "cab": 0
+},
+{
+  
+  "foodname": "Beef",
+  "calorie": 250,
+  "category": "Protein",
+  "protiens": 26,
+  "cab": 0
+},
+{
+  
+  "foodname": "Pork",
+  "calorie": 242,
+  "category": "Protein",
+  "protiens": 26,
+  "cab": 0
+},
+{
+  
+  "foodname": "Lamb",
+  "calorie": 294,
+  "category": "Protein",
+  "protiens": 25,
+  "cab": 0
+},
+{
+ 
+  "foodname": "Shrimp",
+  "calorie": 85,
+  "category": "Protein",
+  "protiens": 20,
+  "cab": 0.2
+},
+{
+ 
+  "foodname": "Quinoa",
+  "calorie": 120,
+  "category": "Grain",
+  "protiens": 4.4,
+  "cab": 21
+},
+{
+ 
+  "foodname": "Brown Rice",
+  "calorie": 216,
+  "category": "Grain",
+  "protiens": 4.5,
+  "cab": 45
+},
+{
+  
+  "foodname": "Oats",
+  "calorie": 389,
+  "category": "Grain",
+  "protiens": 16.9,
+  "cab": 66
+},
+{
+  
+  "foodname": "Quinoa",
+  "calorie": 120,
+  "category": "Grain",
+  "protiens": 4.4,
+  "cab": 21
+},
+{
+ 
+  "foodname": "Bread",
+  "calorie": 265,
+  "category": "Grain",
+  "protiens": 9.4,
+  "cab": 49
+},
+{
+
+  "foodname": "Pasta",
+  "calorie": 131,
+  "category": "Grain",
+  "protiens": 5.5,
+  "cab": 26
+},
+{
+
+  "foodname": "Milk",
+  "calorie": 60,
+  "category": "Dairy",
+  "protiens": 3.2,
+  "cab": 5.1
+},
+{
+
+  "foodname": "Cheese",
+  "calorie": 402,
+  "category": "Dairy",
+  "protiens": 25,
+  "cab": 2.4
+},
+{
+  
+  "foodname": "Yogurt",
+  "calorie": 59,
+  "category": "Dairy",
+  "protiens": 3.5,
+  "cab": 5
+},
+{
+ 
+  "foodname": "Butter",
+  "calorie": 717,
+  "category": "Dairy",
+  "protiens": 0.9,
+  "cab": 0.1
+},
+{
+  
+  "foodname": "Almonds",
+  "calorie": 579,
+  "category": "Nuts",
+  "protiens": 21,
+  "cab": 22
+},
+{
+  
+  "foodname": "Walnuts",
+  "calorie": 654,
+  "category": "Nuts",
+  "protiens": 15,
+  "cab": 14
+},
+{
+  
+  "foodname": "Peanuts",
+  "calorie": 567,
+  "category": "Nuts",
+  "protiens": 26,
+  "cab": 16
+},
+{
+ 
+  "foodname": "Cashews",
+  "calorie": 553,
+  "category": "Nuts",
+  "protiens": 18,
+  "cab": 30
+}
+  
+];
+
+// Task 1: List all the food items
+const listAllFoodItems = () => {
+  return foodData.map(item => item.foodname);
+};
+
+// Task 2: List all the food items with category vegetables
+const listVegetables = () => {
+  return foodData.filter(item => item.category === 'Vegetable');
+};
+
+// Task 3: List all the food items with category fruit
+const listFruits = () => {
+  return foodData.filter(item => item.category === 'Fruit');
+};
+
+// Task 4: List all the food items with category protein
+const listProteins = () => {
+  return foodData.filter(item => item.category === 'Protein');
+};
+
+// Task 5: List all the food items with category nuts
+const listNuts = () => {
+  return foodData.filter(item => item.category === 'Nuts');
+};
+
+// Task 6: List all the food items with category grains
+const listGrains = () => {
+  return foodData.filter(item => item.category === 'Grain');
+};
+
+// Task 7: List all the food items with category dairy
+const listDairy = () => {
+  return foodData.filter(item => item.category === 'Dairy');
+};
+
+// Task 8: List all the food items with calorie above 100
+const listHighCalorie = () => {
+  return foodData.filter(item => item.calorie > 100);
+};
+
+// Task 9: List all the food items with calorie below 100
+const listLowCalorie = () => {
+  return foodData.filter(item => item.calorie < 100);
+};
+
+// Task 10: List all the food items with highest protein content to lowest
+const listFoodItemsByProteinContent = () => {
+  return foodData.slice().sort((a, b) => b.protiens - a.protiens);
+};
+
+// Task 11: List all the food items with lowest cab content to highest
+const listFoodItemsByCarbContent = () => {
+  return foodData.slice().sort((a, b) => a.cab - b.cab);
+};
+console.log("All Food Items:");
+console.log(listAllFoodItems());
+
+console.log("\nVegetables:");
+console.log(listVegetables());
+
+console.log("\nFruits:");
+console.log(listFruits());
+
+console.log("\nProteins:");
+console.log(listProteins());
+
+console.log("\nNuts:");
+console.log(listNuts());
+
+console.log("\nGrains:");
+console.log(listGrains());
+
+console.log("\nDairy:");
+console.log(listDairy());
+
+console.log("\nFood Items with Calorie Above 100:");
+console.log(listHighCalorie());
+
+ console.log("\nFood Items with Calorie Below 100:");
+ console.log(listLowCalorie());
+
+console.log("\nFood Items Sorted by Protein Content (Highest to Lowest):");
+console.log(listFoodItemsByProteinContent());
+
+console.log("\nFood Items Sorted by Cab Content (Lowest to Highest):");
+console.log(listFoodItemsByCarbContent()); 
